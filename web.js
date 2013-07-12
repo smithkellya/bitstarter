5c0156096fb26ce8af2699a97ea933e989ea5a74
Kelly Smith <smithkellya@yahoo.com>
2013-07-12 00:42:16 +0000

    Added index.html content to replace 'Hello World! 2'

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 // I added the next block of code
-var mybuffer = new buffer(fs.readFileSync('~/bitstarter/index.html'));
+var mybuffer = fs.readFileSync('~/bitstarter/index.html');
 // End of block 
 var app = express.createServer(express.logger());
 

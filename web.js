e8a2af3c219bc9294e12bfb8a943ee98b9de4d9a
Kelly Smith <smithkellya@yahoo.com>
2013-07-12 00:54:29 +0000

    Added index.html content to replace 'Hello World! 2'

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-    response.send(mybuffer.toString("utf-8"));
+    response.send(mybuffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;

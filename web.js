00e539521f43c4c21b35d821f0fe43ed651aeebe
Kelly Smith <smithkellya@yahoo.com>
2013-07-12 00:06:08 +0000

    Added index.html content to replace 'Hello World! 2'

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+// I added the next block of code
+var mybuffer = new buffer(fs.readFileSync('~/bitstarter/index.html'));
+// End of block 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World 2!');
+    response.send(mybuffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
